8da70df2166784b023a127efafb2aaf6ac325733
Hyoseung Jeon <qmflwj1441@gmail.com>
2021-02-02 18:21:58 +0900

    Add Vue

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    devServer:{
+      proxy:{
+        '/' :{
+          target: 'http://localhost:8080',
+          ws: true,
+          changeOrigin: true
+        },
+      }
+    }
+  }
